2accbae70028f8a9038fa1d7b8ca85e21bf2a186
Xanderyeng <50344667+Xanderyeng@users.noreply.github.com>
2023-03-19 19:31:09 +0300

    venom-bg

--- src/containers/Hero/Hero.jsx
+++ src/containers/Hero/Hero.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Button, Container } from '@mui/material'
-import bg from '../../assets/bg.png'
+// import bg from '../../assets/bg.png'
 import './hero.css'
 
 const Hero = () => {
   return (
     <section className="hero-section">
-    <img src={bg} className='bg'/>
+    {/* <img src={bg} className='bg'/> */}
     <Container maxWidth="xl lg" sx={{ position: 'relative', top: '50%', left: '0', zIndex: '1' }}>
         <div className="title">
             <div className="text1">venom</div>
